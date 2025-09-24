62f857321c839fd8b62da96adbdcbeee0da27cae
PranavBj2406 <pranavbj80@gmail.com>
2025-09-24 12:47:32 +0530

    RESTART OF ML_SERVICE

--- server/ml_service/main.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-from api.api import app   # import the FastAPI app already defined in api/api.py
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
